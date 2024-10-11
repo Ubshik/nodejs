6c56936b8f8245280e73c01edfa93a58697f0f7b
lubov.vol <ubshik@gmail.com>
2024-10-11 19:39:30 +0300

    remove unrunning version

--- bank_account/routes/dashboardRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import jwtMiddleware from '../middlewares/jwtMiddleware.js';
-import dashboardController from '../controllers/dashboardController.js';
-
-const router = express.Router();
-
-router.get('/', jwtMiddleware.verifyToken, dashboardController.getDashboard);
-
-export default router;
--- bank_account/routes/logoutRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import logOutController from '../controllers/logOutController.js';
-
-const router = express.Router();
-
-router.delete('/', logOutController.logOut);
-
-export default router;
