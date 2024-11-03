09e92da5d2d0c9942285df99dd30a13513febeba
lubov.vol <ubshik@gmail.com>
2024-11-03 13:30:42 +0200

    correct static frontend for the docker container

--- bank_account/src/app.js
+++ bank_account/src/app.js
@@ -7,6 +7,8 @@ import dashboardRouter from './routes/dashboardRouter.js';
 import transactionRouter from './routes/transactionRouter.js';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import path from 'path'; //for static pages for dcocker (for import)
+import { fileURLToPath  } from 'url'; //for static pages for dcocker
 
 // const express = require('express');
 const app = express();
@@ -14,12 +16,21 @@ const PORT = process.env.APP_PORT;
 
 app.use(cors());
 app.use(express.static("frontend"));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use('/static', express.static(path.join(__dirname, '..', 'frontend')));
+
 app.use(express.json());
 app.use('/api/v1/signup', signupRouter);
 app.use('/api/v1/login', loginRouter);
 app.use('/api/v1/logout', logoutRouter);
 app.use('/api/v1/dashboard', dashboardRouter);
 app.use('/api/v1/transaction', transactionRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log("server is listening on port " + PORT);
@@ -28,13 +39,13 @@ app.listen(PORT, () => {
 dotenv.config();
 
 // !!! for local machine
-const dbURL = 'mongodb://' + process.env.DATABASE_HOST + 
-                ':' + process.env.DATABASE_PORT + 
-                '/' +  process.env.DATABASE_NAME +
-                '?directConnection=true&replicaSet=replicaset&retryWrites=true';
+// const dbURL = 'mongodb://' + process.env.DATABASE_HOST + 
+//                 ':' + process.env.DATABASE_PORT + 
+//                 '/' +  process.env.DATABASE_NAME +
+//                 '?directConnection=true&replicaSet=replicaset&retryWrites=true';
 
 // !!! for docker compose
-// const dbURL = 'mongodb://admin-123:password-123@mongodb-primary:27017/bank_app?authSource=bank_app';
+const dbURL = 'mongodb://admin-123:password-123@mongodb-primary:27017/bank_app?authSource=bank_app';
 
 console.log("mongo_url: " + dbURL);
 
